a1a49c59f1802d8c572d63baa4b2e3b9cd7503d7
sandybest4path <sezeh@byupathway.edu>
2025-03-14 15:59:07 +0100

    w01/Exercise1/program.cs

--- Program.cs
+++ Program.cs
@@ -2,41 +2,14 @@
 
 class Program
 {
-    // Function to display a welcome message
-    static void DisplayWelcome()
-    {
-        Console.WriteLine("Welcome to the Program!");
-    }
-
-    static string PromptUserName()
-    {
-        Console.Write("Please enter your name: ");
-        return Console.ReadLine();
-    }
-
-    static int PromptUserNumber()
-    {
-        Console.Write("Please enter your favorite number: ");
-        return int.Parse(Console.ReadLine());
-    }
-
-    static int SquareNumber(int number)
-    {
-        return number * number;
-    }
-
-    static void DisplayResult(string name, int squaredNumber)
-    {
-        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
-    }
-
     static void Main(string[] args)
     {
-        DisplayWelcome();
-        string userName = PromptUserName();
-        int userNumber = PromptUserNumber();
-        int squaredNumber = SquareNumber(userNumber);
-        DisplayResult(userName, squaredNumber);
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
-
