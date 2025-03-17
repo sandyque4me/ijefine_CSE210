af81a67cf04bd7b1b499ed76b14636fb348770b4
Sandra Ezeh <sandybest4lifes@gmail.com>
2025-03-17 15:01:10 +0100

    Added changes

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //I asked the user for name.
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
